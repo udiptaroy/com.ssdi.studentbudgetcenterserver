959beacae21b03c797bc20c7441ad3d3674c4393
Gipionocheiyort <pandrewsdev@gmail.com>
2014-12-03 13:09:27 -0500

    Phil Commit Fix
    Forgot that there were some small changes to the two entity classes

--- src/com/ssdi/studentbudgetcenterentity/FriendRequest.java
+++ src/com/ssdi/studentbudgetcenterentity/FriendRequest.java
@@ -5,30 +5,46 @@ public class FriendRequest {
 	private User sentTo;
 	private char status;
 	private String createDate;
+	private int RequestId;
+
+	public int getRequestId() {
+		return RequestId;
+	}
+
+	public void setRequestId(int requestId) {
+		RequestId = requestId;
+	}
+
 	public User getSentBy() {
 		return sentBy;
 	}
+
 	public void setSentBy(User sentBy) {
 		this.sentBy = sentBy;
 	}
+
 	public User getSentTo() {
 		return sentTo;
 	}
+
 	public void setSentTo(User sentTo) {
 		this.sentTo = sentTo;
 	}
+
 	public char getStatus() {
 		return status;
 	}
+
 	public void setStatus(char status) {
 		this.status = status;
 	}
+
 	public String getCreateDate() {
 		return createDate;
 	}
+
 	public void setCreateDate(String createDate) {
 		this.createDate = createDate;
 	}
-	
 
 }
--- src/com/ssdi/studentbudgetcenterentity/TransferRequest.java
+++ src/com/ssdi/studentbudgetcenterentity/TransferRequest.java
@@ -5,30 +5,46 @@ public class TransferRequest {
 	private User sentTo;
 	private char status;
 	private double transferAmt;
+	private int RequestId;
+
+	public int getRequestId() {
+		return RequestId;
+	}
+
+	public void setRequestId(int requestId) {
+		RequestId = requestId;
+	}
+
 	public User getSentFrom() {
 		return sentFrom;
 	}
+
 	public void setSentFrom(User sentFrom) {
 		this.sentFrom = sentFrom;
 	}
+
 	public User getSentTo() {
 		return sentTo;
 	}
+
 	public void setSentTo(User sentTo) {
 		this.sentTo = sentTo;
 	}
+
 	public char getStatus() {
 		return status;
 	}
+
 	public void setStatus(char status) {
 		this.status = status;
 	}
+
 	public double getTransferAmt() {
 		return transferAmt;
 	}
+
 	public void setTransferAmt(double transferAmt) {
 		this.transferAmt = transferAmt;
 	}
-	
 
 }
